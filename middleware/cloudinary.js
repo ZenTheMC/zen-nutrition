57f9a430387a54fe8344918a5ec114d5245b7fe5
Amaar A <amaar5ali@gmail.com>
2023-11-08 15:14:01 -0500

    Update cloudinary.js

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,4 +1,4 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary").v2; // Is this the correct version? Check if outdated or if version is correct
 
 require("dotenv").config({ path: "./config/.env" });
 
